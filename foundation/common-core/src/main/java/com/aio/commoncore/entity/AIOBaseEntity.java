7e63e340bae6d7716ae5e15db87fe0a86c1fc478
Nguyencoder11 <lenhnguyen10a22003@gmail.com>
2026-04-18 21:24:31 +0700

    :fire: Refactor modules

---
+++ foundation/common-core/src/main/java/com/aio/commoncore/entity/AIOBaseEntity.java
@@ -0,0 +1,16 @@
+/*
+ * AIOBaseEntity.java
+ *
+ * Copyright (c) 2025 Nguyen. All rights reserved.
+ * This software is the confidential and proprietary information of Nguyen.
+ */
+
+package com.aio.commoncore.entity;
+
+/**
+ * AIOBaseEntity.java
+ *
+ * @author Nguyen
+ */
+public abstract class AIOBaseEntity {
+}
